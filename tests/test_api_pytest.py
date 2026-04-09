a091df8e850b54bb432c1993a8541d5b70d894bc
Jessica Elisa de Oliveira <jessica.oliveira@compasso.com.br>
2026-04-09 08:08:48 -0300

    test: adiciona teste dummy para trigger do PR

--- tests/test_api_pytest.py
+++ tests/test_api_pytest.py
@@ -19,3 +19,8 @@ def test_get_tasks(client):
 
     assert response.status_code == 200
     assert isinstance(response.json, list)
+    
+    
+    # adiciona isso
+def test_dummy():
+    assert True
